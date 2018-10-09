bb253bdb954abd42115f5d2fd9f32ff65b1529c0
jebax <joel.eliot.baxter@gmail.com>
2018-10-09 11:30:41 +0100

    refactored tests, added support for conjured items

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,6 +1,7 @@
 class GildedRose
   SPECIAL = ["Aged Brie",
              "Sulfuras, Hand of Ragnaros",
+             "Conjured",
              "Backstage passes to a TAFKAL80ETC concert"]
 
   def initialize(items)
@@ -11,8 +12,7 @@ class GildedRose
   def update_quality()
     @items.each do |item|
       item.sell_in -= 1 unless item.name == "Sulfuras, Hand of Ragnaros"
-      special_update(item) if SPECIAL.include? item.name
-      normal_update(item) unless SPECIAL.include? item.name
+      special_check(item.name) ? special_update(item) : normal_update(item)
     end
     quality_bounds_check(@items)
   end
@@ -23,9 +23,14 @@ class GildedRose
     item.sell_in > 0 ? item.quality -= 1 : item.quality -= 2
   end
 
+  def special_check(name)
+    SPECIAL.include?(name) || name.match(/Conjured/)
+  end
+
   def special_update(item)
     brie_update(item) if item.name == SPECIAL.first
     passes_update(item) if item.name == SPECIAL.last
+    conjured_update(item) if item.name.match(/Conjured/)
   end
 
   def brie_update(item)
@@ -34,10 +39,14 @@ class GildedRose
   end
 
   def passes_update(item)
-    item.quality += 1 if item.sell_in > 10
-    item.quality += 2 if item.sell_in <= 10
+    item.quality += 1
+    item.quality += 1 if item.sell_in <= 10
     item.quality += 1 if item.sell_in <= 5
     item.quality = 0 if item.sell_in <= 0
+  end
+
+  def conjured_update(item)
+    item.sell_in > 0 ? item.quality -= 2 : item.quality -= 4
   end
 
   def quality_bounds_check(items)
--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -5,6 +5,7 @@ require 'pry'
 describe GildedRose do
   let(:special_items) { [Item.new("Aged Brie", 5, 45),
                          Item.new("Sulfuras, Hand of Ragnaros", 5, 45),
+                         Item.new("Conjured", 5, 45),
                          Item.new("Backstage passes to a TAFKAL80ETC concert", 20, 45)]
   }
 
@@ -26,10 +27,74 @@ describe GildedRose do
     end
   end
 
-  context "item is special" do
-    it 'should have an unchanging list of special items' do
-      expect(described_class::SPECIAL).to eq special_items.map(&:name)
+  it { expect(described_class::SPECIAL).to eq special_items.map(&:name) }
+
+  context "#item is conjured" do
+     it 'should reduce conjured quality by -2 before sell_in date' do
+       items = [Item.new("Conjured Mana Cake", 5, 45)]
+       GildedRose.new(items).update_quality
+       expect(items.first.quality).to eq 43
+     end
+
+     it 'should reduce quality by -4 after sell_in date' do
+       items = [Item.new("Conjured Mana Cake", -2, 45)]
+       GildedRose.new(items).update_quality
+       expect(items.first.quality).to eq 41
+     end
+  end
+
+  context "#item is sulfuras, hand of ragnaros" do
+    it 'should not update quality' do
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 5, 45)]
+      GildedRose.new(items).update_quality
+      expect(items.first.quality).to eq 45
     end
+
+    it 'should not update sell_in' do
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 5, 45)]
+      GildedRose.new(items).update_quality
+      expect(items.first.sell_in).to eq 5
+    end
+  end
+
+  context "item is aged brie" do
+    it 'should update brie quality by +1 if sell_in has not passed' do
+      items = [Item.new("Aged Brie", 5, 45)]
+      GildedRose.new(items).update_quality
+      expect(items.first.quality).to eq 46
+    end
+
+    it 'should update brie quality by +2 if sell_in has passed' do
+      items = [Item.new("Aged Brie", -2, 45)]
+      GildedRose.new(items).update_quality
+      expect(items.first.quality).to eq 47
+    end
+  end
+
+  context "#item is a backstage pass" do
+    it 'should increase quality of pass with 20 days before sell_in by 1' do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 20, 45)]
+      GildedRose.new(items).update_quality
+      expect(items.first.quality).to eq 46
+    end
+
+     it 'should increase quality of pass with 10 days before sell_in by 2' do
+       items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 45)]
+       GildedRose.new(items).update_quality
+       expect(items.first.quality).to eq 47
+     end
+
+     it 'should increase quality of pass with 5 days before sell_in by 3' do
+       items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 45)]
+       GildedRose.new(items).update_quality
+       expect(items.first.quality).to eq 48
+     end
+
+     it 'should set quality of pass to 0 if sell_in date has passed' do
+       items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 45)]
+       GildedRose.new(items).update_quality
+       expect(items.first.quality).to eq 0
+     end
   end
 
   context "item is normal" do
@@ -64,31 +129,4 @@ describe GildedRose do
       expect(items.first.quality).to eq 0
     end
   end
-
-  describe "#passes_update" do
-    it 'should increase quality of pass with 20 days before sell_in by 1' do
-      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 20, 45)]
-      GildedRose.new(items).update_quality
-      expect(items.first.quality).to eq 46
-    end
-
-     it 'should increase quality of pass with 10 days before sell_in by 2' do
-       items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 45)]
-       GildedRose.new(items).update_quality
-       expect(items.first.quality).to eq 47
-     end
-
-     it 'should increase quality of pass with 5 days before sell_in by 3' do
-       items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 45)]
-       GildedRose.new(items).update_quality
-       expect(items.first.quality).to eq 48
-     end
-
-     it 'should set quality of pass to 0 if sell_in date has passed' do
-       items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 45)]
-       GildedRose.new(items).update_quality
-       expect(items.first.quality).to eq 0
-     end
-  end
-
 end
